eb3961f7148744f64f1e1eb9cb54bc9e0b560a05
under-44 <benhootnz@gmail.com>
2022-11-04 11:20:27 +1300

    modified create_path's content to add byte support

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//-----> global contants
+//-----> global constants
 
 pub const HTML_DEFAULT: &str = "<!DOCTYPE html>\n    <html lang=\"en\">\n    \n    <head>\n      <meta charset=\"UTF-8\">\n      <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n      <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n      <link rel=\"stylesheet\" href=\"./styles/style.css\">\n      <title>Document</title>\n    </head>\n    \n    <body>\n    \n      <script src=\"./javascript/index.js\"></script>\n    </body>\n    \n    </html>\n    ";
 pub const JS_DEFAULT: &str = "// \u{2193} do stuff \u{2193}\n    ";
@@ -9,18 +9,19 @@ pub const CSS_DEFAULT: &str = "/* \u{2193} do stuff \u{2193} */\n    ";
 /**
  ## path to a file with optional content
 
- the content takes a `Option<T>` pass `None`\
- if you want no content in that file
+ content takes a `Option<&[u8]>` aka bytes\
+ pass `None` for no file contents
 
  ---
  ### example
  ```
 //          ↓ path             ↓ content
-create_path("folder/file.txt", Some("Hello, world!"));
+create_path("folder/file.txt", Some(b"Hello, world!"));
  ```
 
 */
-pub fn create_path(path: &str, content: Option<&str>) -> Result<(), std::io::Error>{
+pub fn create_path(path: &str, content: Option<&[u8]>) -> Result<(), std::io::Error>
+{
     //  ↓ split so we can pop last for creating folders
     let file_path = path.split("/").collect::<Vec<&str>>();
     //  ↓ get file name at end of path for bellow
@@ -29,9 +30,7 @@ pub fn create_path(path: &str, content: Option<&str>) -> Result<(), std::io::Err
     let folder = path.to_owned().replace(&file, "");
 
     // ↓ create folders
-    std::fs::DirBuilder::new()
-        .recursive(true)
-        .create(&folder)?;
+    std::fs::DirBuilder::new().recursive(true).create(&folder)?;
 
     // ↓ create file
     let mut file = std::fs::OpenOptions::new()
@@ -39,9 +38,10 @@ pub fn create_path(path: &str, content: Option<&str>) -> Result<(), std::io::Err
         .create(true)
         .open(path)?;
 
+    
     use std::io::Write;
     //                     ↓ if none default to no write
-    file.write_all(content.unwrap_or("").as_bytes())?;
+    file.write_all(content.unwrap_or(&[0]))?;
 
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@ fn main() {
     // ↓ create files
     let args: Vec<String> = std::env::args().collect();
     if args.len() > 1 {
-        jhc::create_path(&format!("{}/index.html", &args[1]), Some(jhc::HTML_DEFAULT)).unwrap();
-        jhc::create_path(&format!("{}/styles/style.css", &args[1]), Some(jhc::CSS_DEFAULT)).unwrap();
-        jhc::create_path(&format!("{}/javascript/index.js", &args[1]), Some(jhc::JS_DEFAULT)).unwrap();
+        jhc::create_path(&format!("{}/index.html", &args[1]), Some(jhc::HTML_DEFAULT.as_bytes())).unwrap();
+        jhc::create_path(&format!("{}/styles/style.css", &args[1]), Some(jhc::CSS_DEFAULT.as_bytes())).unwrap();
+        jhc::create_path(&format!("{}/javascript/index.js", &args[1]), Some(jhc::JS_DEFAULT.as_bytes())).unwrap();
         return;
     }
-    jhc::create_path("index.html", Some(jhc::HTML_DEFAULT)).unwrap();
-    jhc::create_path("styles/style.css", Some(jhc::CSS_DEFAULT)).unwrap();
-    jhc::create_path("javascript/index.js", Some(jhc::JS_DEFAULT)).unwrap();
+    jhc::create_path("index.html", Some(jhc::HTML_DEFAULT.as_bytes())).unwrap();
+    jhc::create_path("styles/style.css", Some(jhc::CSS_DEFAULT.as_bytes())).unwrap();
+    jhc::create_path("javascript/index.js", Some(jhc::JS_DEFAULT.as_bytes())).unwrap();
 }
